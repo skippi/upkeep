3556d0976e0041ebdf6c936b99f1add77f5aaaf3
skippi <jbtcao@gmail.com>
2022-03-10 21:58:17 -0600

    feat(task): add input options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import React, { useState } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const [tasks, setTasks] = useState([]);
   return (
     <div className="App">
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setTasks([...tasks, name])}>Enter</button>
+      {tasks.map((task, idx) => (
+        <div>
+          <span key={idx}>{task}</span>
+        </div>
+      ))}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
