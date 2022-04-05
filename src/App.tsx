7e49a9d55ef41d32b2f97e62fadf595b7bd8cbd3
skippi <jbtcao@gmail.com>
2022-04-04 22:18:13 -0500

    fix(app): break app jsx into smaller components

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ import {
 import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 import {
+  Action,
+  AppState,
   appReducer,
   elapsedTimeSession,
   elapsedTimeTask,
@@ -41,10 +43,6 @@ function msToHHMMSS(milliseconds: number) {
 function App() {
   const [name, setName] = useState<string>("");
   const [app, dispatch] = useReducer(appReducer, initialState);
-  const [taskInfoVisible, setTaskInfoVisible] = useState<number[]>([]);
-  const [notesVisible, setNotesVisible] = useState<number[]>([]);
-  const [historyVisible, setHistoryVisible] = useState<number[]>([]);
-  const [completionsVisible, setCompletionsVisible] = useState<number[]>([]);
   useEffect(() => {
     const interval = setInterval(() => dispatch({ type: "refresh" }), 1000);
     return () => clearInterval(interval);
@@ -100,217 +98,7 @@ function App() {
         {Object.values(app.tasks).map((task, i) => (
           <Box key={task.id}>
             {i !== 0 && <Divider component="li" />}
-            <ListItem
-              secondaryAction={
-                <Box>
-                  <IconButton
-                    onClick={() =>
-                      dispatch({ type: "toggleClockTask", id: task.id })
-                    }
-                  >
-                    {!isClockedInTask(app, task.id) && <PlayArrowIcon />}
-                    {isClockedInTask(app, task.id) && <PauseIcon />}
-                  </IconButton>
-                  <IconButton
-                    onClick={() => {
-                      if (taskInfoVisible.includes(task.id)) {
-                        setTaskInfoVisible(
-                          taskInfoVisible.filter((i) => i !== task.id)
-                        );
-                        return;
-                      }
-                      setTaskInfoVisible([...taskInfoVisible, task.id]);
-                    }}
-                  >
-                    {!taskInfoVisible.includes(task.id) ? (
-                      <ArrowDropDownIcon />
-                    ) : (
-                      <ArrowDropUpIcon />
-                    )}
-                  </IconButton>
-                </Box>
-              }
-            >
-              <ListItemText
-                sx={{
-                  color: isClockedInTask(app, task.id) ? "green" : undefined,
-                }}
-              >
-                {task.name}
-              </ListItemText>
-            </ListItem>
-            {taskInfoVisible.includes(task.id) && (
-              <Box component="li">
-                <Box>
-                  Scheduled: {task.scheduleDate?.toISOString()}
-                  <IconButton
-                    onClick={() =>
-                      dispatch({ type: "rescheduleTask", id: task.id })
-                    }
-                  >
-                    <CalendarMonthIcon />
-                  </IconButton>
-                </Box>
-                <Box>
-                  Time Taken:&nbsp;
-                  <Typography
-                    component="span"
-                    style={{
-                      color:
-                        elapsedTimeTask(app, task.id) > task.estimate
-                          ? "red"
-                          : undefined,
-                    }}
-                  >
-                    {msToHHMMSS(elapsedTimeTask(app, task.id))}
-                  </Typography>
-                </Box>
-                <Box>
-                  Estimate:&nbsp;
-                  <Input
-                    value={msToHHMMSS(task.estimate)}
-                    onChange={(e) => {
-                      let parsed = parseInt(e.target.value);
-                      if (isNaN(parsed)) {
-                        parsed = 0;
-                      }
-                      dispatch({
-                        type: "estimateTask",
-                        id: task.id,
-                        length: parsed * 1000,
-                      });
-                    }}
-                  />
-                </Box>
-                <button
-                  onClick={() => {
-                    if (notesVisible.includes(task.id)) {
-                      setNotesVisible(
-                        notesVisible.filter((i) => i !== task.id)
-                      );
-                      return;
-                    }
-                    setNotesVisible([...notesVisible, task.id]);
-                  }}
-                >
-                  N
-                </button>
-                <button
-                  onClick={() => dispatch({ type: "closeTask", id: task.id })}
-                >
-                  C
-                </button>
-                <button
-                  onClick={() => {
-                    if (historyVisible.includes(task.id)) {
-                      setHistoryVisible(
-                        historyVisible.filter((i) => i !== task.id)
-                      );
-                      return;
-                    }
-                    setHistoryVisible([...historyVisible, task.id]);
-                  }}
-                >
-                  History
-                </button>
-                <button
-                  onClick={() => {
-                    dispatch({ type: "deleteTask", id: task.id });
-                    setNotesVisible(notesVisible.filter((i) => i !== task.id));
-                  }}
-                >
-                  X
-                </button>
-                {notesVisible.includes(task.id) && (
-                  <div>
-                    <textarea
-                      onChange={(e) =>
-                        dispatch({
-                          type: "editTaskNotes",
-                          id: task.id,
-                          value: e.target.value,
-                        })
-                      }
-                    />
-                  </div>
-                )}
-                {historyVisible.includes(task.id) && (
-                  <div key={task.id}>
-                    <div>
-                      Total Time: {msToHHMMSS(totalTimeTask(app, task.id))}
-                    </div>
-                    <div>
-                      <div>Completions</div>
-                      {task.completions
-                        .map((cid) => app.completions[cid])
-                        .map((completion) => (
-                          <div
-                            key={JSON.stringify([
-                              "completion",
-                              task.id,
-                              completion.id,
-                            ])}
-                          >
-                            {completion.date.toISOString()}
-                            <button
-                              onClick={() => {
-                                if (
-                                  completionsVisible.includes(completion.id)
-                                ) {
-                                  setCompletionsVisible(
-                                    completionsVisible.filter(
-                                      (a) => a !== completion.id
-                                    )
-                                  );
-                                  return;
-                                }
-                                setCompletionsVisible([
-                                  ...completionsVisible,
-                                  completion.id,
-                                ]);
-                              }}
-                            >
-                              Notes
-                            </button>
-                            {completionsVisible.includes(completion.id) && (
-                              <div>
-                                <textarea
-                                  onChange={(e) =>
-                                    dispatch({
-                                      type: "editCompletionNotes",
-                                      id: completion.id,
-                                      value: e.target.value,
-                                    })
-                                  }
-                                  value={completion.notes}
-                                />
-                              </div>
-                            )}
-                          </div>
-                        ))}
-                    </div>
-                    <div>
-                      <div>Sessions</div>
-                      {task.sessions
-                        .map((sid) => app.sessions[sid])
-                        .map((session) => (
-                          <div
-                            key={JSON.stringify([
-                              "session",
-                              task.id,
-                              session.id,
-                            ])}
-                          >
-                            {session.start.toISOString()} -{" "}
-                            {session.end?.toISOString() ?? ""} |{" "}
-                            {msToHHMMSS(elapsedTimeSession(session))}
-                          </div>
-                        ))}
-                    </div>
-                  </div>
-                )}
-              </Box>
-            )}
+            <TaskViewItem id={task.id} app={app} dispatch={dispatch} />
           </Box>
         ))}
       </List>
@@ -334,6 +122,174 @@ function App() {
           .substring(11, 19)}
       </div>
     </Paper>
+  );
+}
+
+function CompletionViewItem(props: {
+  id: number;
+  app: AppState;
+  dispatch: (action: Action) => void;
+}) {
+  const { id, app, dispatch } = props;
+  const [notesVisible, setNotesVisible] = useState<Boolean>(false);
+  const completion = app.completions[id];
+  return (
+    <div>
+      {completion.date.toISOString()}
+      <button onClick={() => setNotesVisible(!notesVisible)}>Notes</button>
+      {notesVisible && (
+        <div>
+          <textarea
+            onChange={(e) =>
+              dispatch({
+                type: "editCompletionNotes",
+                id: completion.id,
+                value: e.target.value,
+              })
+            }
+            value={completion.notes}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function TaskViewItem(props: {
+  id: number;
+  app: AppState;
+  dispatch: (action: Action) => void;
+}) {
+  const { id, app, dispatch } = props;
+  const [infoVisible, setInfoVisible] = useState<Boolean>(false);
+  const [notesVisible, setNotesVisible] = useState<Boolean>(false);
+  const [historyVisible, setHistoryVisible] = useState<Boolean>(false);
+  const task = app.tasks[id];
+  const clockedIn = isClockedInTask(app, id);
+  const overdue = elapsedTimeTask(app, id) > task.estimate;
+  return (
+    <Box>
+      <ListItem
+        secondaryAction={
+          <Box>
+            <IconButton
+              onClick={() => dispatch({ type: "toggleClockTask", id: task.id })}
+            >
+              {!clockedIn && <PlayArrowIcon />}
+              {clockedIn && <PauseIcon />}
+            </IconButton>
+            <IconButton onClick={() => setInfoVisible(!infoVisible)}>
+              {!infoVisible ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+            </IconButton>
+          </Box>
+        }
+      >
+        <ListItemText
+          sx={{
+            color: clockedIn ? "green" : undefined,
+          }}
+        >
+          {task.name}
+        </ListItemText>
+      </ListItem>
+      {infoVisible && (
+        <Box component="li">
+          <Box>
+            Scheduled: {task.scheduleDate?.toISOString()}
+            <IconButton
+              onClick={() => dispatch({ type: "rescheduleTask", id: task.id })}
+            >
+              <CalendarMonthIcon />
+            </IconButton>
+          </Box>
+          <Box>
+            Time Taken:&nbsp;
+            <Typography
+              component="span"
+              style={{
+                color: overdue ? "red" : undefined,
+              }}
+            >
+              {msToHHMMSS(elapsedTimeTask(app, task.id))}
+            </Typography>
+          </Box>
+          <Box>
+            Estimate:&nbsp;
+            <Input
+              value={msToHHMMSS(task.estimate)}
+              onChange={(e) => {
+                let parsed = parseInt(e.target.value);
+                if (isNaN(parsed)) {
+                  parsed = 0;
+                }
+                dispatch({
+                  type: "estimateTask",
+                  id: task.id,
+                  length: parsed * 1000,
+                });
+              }}
+            />
+          </Box>
+          <button onClick={() => setNotesVisible(!notesVisible)}>N</button>
+          <button onClick={() => dispatch({ type: "closeTask", id: task.id })}>
+            C
+          </button>
+          <button onClick={() => setHistoryVisible(!historyVisible)}>
+            History
+          </button>
+          <button onClick={() => dispatch({ type: "deleteTask", id: task.id })}>
+            X
+          </button>
+          {notesVisible && (
+            <div>
+              <textarea
+                onChange={(e) =>
+                  dispatch({
+                    type: "editTaskNotes",
+                    id: task.id,
+                    value: e.target.value,
+                  })
+                }
+              />
+            </div>
+          )}
+          {historyVisible && (
+            <div key={task.id}>
+              <div>Total Time: {msToHHMMSS(totalTimeTask(app, task.id))}</div>
+              <div>
+                <div>Completions</div>
+                {task.completions
+                  .map((cid) => app.completions[cid])
+                  .map((completion) => (
+                    <CompletionViewItem
+                      key={JSON.stringify([
+                        "completion",
+                        task.id,
+                        completion.id,
+                      ])}
+                      id={completion.id}
+                      app={app}
+                      dispatch={dispatch}
+                    />
+                  ))}
+              </div>
+              <div>
+                <div>Sessions</div>
+                {task.sessions
+                  .map((sid) => app.sessions[sid])
+                  .map((session) => (
+                    <div key={JSON.stringify(["session", task.id, session.id])}>
+                      {session.start.toISOString()} -{" "}
+                      {session.end?.toISOString() ?? ""} |{" "}
+                      {msToHHMMSS(elapsedTimeSession(session))}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
+        </Box>
+      )}
+    </Box>
   );
 }
 
